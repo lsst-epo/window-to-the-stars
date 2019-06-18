914437401e76434a4b88c024d86f17fc6eac71c5
Blake Mason <bmason@lsst.org>
2019-06-17 18:20:12 -0700

    Updates

---
+++ templates/src/pages/about.d4401ee8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{56:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a);t.default=function(){return r.a.createElement("div",null,r.a.createElement("p",null,"React Static is a progressive static site generator for React."))}}}]);
+//# sourceMappingURL=about.d4401ee8.js.map
---
+++ templates/src/pages/index.147aebc5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{54:function(e,t,n){"use strict";n.r(t);var c=n(0),a=n.n(c);t.default=function(){return a.a.createElement("div",{style:{textAlign:"center"}},a.a.createElement("h1",null,"Welcome to React-Static"))}}}]);
+//# sourceMappingURL=index.147aebc5.js.map
